df68c04baf7c7460e8cd53ec274d81d86383167d
T. Mjoen <trond@twined.net>
2025-08-02 13:01:10 +0200

    update formatter

--- .formatter.exs
+++ .formatter.exs
@@ -1,4 +1,13 @@
-# Used by "mix format"
+# we export florist's locals_without_parens here
+locals_without_parens = [
+  set: 2,
+  target: 2,
+  project_module: 1,
+  project_name: 1
+]
+
 [
+  locals_without_parens: locals_without_parens,
+  export: [locals_without_parens: locals_without_parens],
   inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"]
 ]
